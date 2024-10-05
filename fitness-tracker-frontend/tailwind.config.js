18c3c6b6e17dd9855d21c277703e743093718305
felixsteinke <steinke.felix@yahoo.de>
2024-10-05 19:47:18 +0200

    #2: update tailwind content identifier

--- fitness-tracker-frontend/tailwind.config.js
+++ fitness-tracker-frontend/tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    // TODO #2: configure custom content according to https://www.nativewind.dev/quick-starts/react-native-cli
-    content: ["./App.{js,jsx,ts,tsx}", "./<custom-folder>/**/*.{js,jsx,ts,tsx}"],
+    content: ["./{app,components}/**/*.{js,jsx,ts,tsx}"],
     theme: {
         extend: {},
     },
