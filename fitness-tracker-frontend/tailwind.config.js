a72872538ae03713b99eb5c83cf0aa6a2951bb9a
felixsteinke <steinke.felix@yahoo.de>
2024-10-05 18:50:38 +0200

    #2: add tailwind dependencies

---
+++ fitness-tracker-frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    // TODO #2: configure custom content according to https://www.nativewind.dev/quick-starts/react-native-cli
+    content: ["./App.{js,jsx,ts,tsx}", "./<custom-folder>/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
